8264d94ee7ef0aef1001855291066d707d4888a0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-06-18 09:19:46 +0900

    usersとuser1のボタン準備

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles.css";
 
 export default function App() {
+  const onClickUsers = () => {
+    alert("users");
+  };
+  const onClickUser1 = () => {
+    alert("user1");
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Edit to see some magic happen!</h2>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
     </div>
   );
 }
